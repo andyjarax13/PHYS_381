216a3cbb68d1e856ddb12b6b7850bd0fa0612cab
Andres Jaramillo <andresjaramillofigueroa@gmail.com>
2020-02-10 20:16:09 -0700

    Fixed data errors

--- lab1.py
+++ lab1.py
@@ -6,20 +6,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("lab1.csv")
-df = df[df["Direction"] == " P"]
-R = df["R"]
-I = df["I"]
-I_e = df["I_e"]
-V = df["V"]
-V_e = df["V_e"]
+df = df[df["Direction"] == " A"]
+df_A = df[df["Direction"] == " A"]
+df_P = df[df["Direction"] == " P"]
 
-print(df)
+R = df["R"]
+R_A = df_A["R"]
+R_P = df_P["R"]
+
+I = df["I"]
+I_A = df_A["I"]
+I_P = df_P["I"]
+
+I_e = df["I_e"]
+I_e_A = df_A["I_e"]
+I_e_P = df_P["I_e"]
+
+V = df["V"]
+V_A = df_A["V"]
+V_P = df_P["V"]
+
+V_e = df["V_e"]
+V_e_A = df_A["V_e"]
+V_e_P = df_P["V_e"]
 
 N = 72 # Coils
 a = 0.33 # Radius of coil
-Be = 36.11e-6 # [T]
+Be = -36.11e-6 # [T]
+Be_e = 2e-6
 Bh = (8*c.mu_0*N*I)/((125)**(1/2)*a)
 x = (R**2*(Be + Bh)**2)/2
+x_e = (   (I_e**2)*(R**2*(Bh/I)*(Bh+Be))**2 + (Be_e**2)*(R**2*(Bh+Be))**2   )**(1/2)
 e_m = V/x
 e_m_actual = 1.758820e11
 error = (e_m - e_m_actual)/e_m_actual
@@ -27,13 +44,13 @@ error = (e_m - e_m_actual)/e_m_actual
 print(e_m, error)
 
 plt.figure(figsize=(10,5))
-plt.plot(x, V, ".")
-#plt.errorbar(x, V, I_e, I_e, "r-o", ecolor="blue", capsize=10)
+#plt.plot(x, V, ".")
+plt.errorbar(x, V, V_e, x_e, ".r", ecolor="blue", capsize=1)
 #plt.xscale("log")
 #plt.yscale("log")
 plt.title("Collector Curve for $I_b$")
-plt.xlabel("$V_{ce}~[V]$")
-plt.ylabel("$I_{c}~[A]$")
+plt.xlabel("$x_{ce}~[V]$")
+plt.ylabel("$V_{accel}~[V]$")
 plt.grid()
 plt.tight_layout()
 plt.savefig("lab1_1")
